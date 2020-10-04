5eb1359c46e1c8cfec05d92d038c80d20a02f54c
islandvalley <osm.shimaya@gmail.com>
2020-10-04 15:37:05 +0900

    工事中画面

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
 const Home = () => {
-  return <div>hello world</div>
+  return (
+    <>
+      <main className="main">
+        <h1>🚧工事中</h1>
+      </main>
+      <style jsx>{`
+        .main {
+          align-items: center;
+          display: flex;
+          height: 100vh;
+          justify-content: center;
+        }
+      `}</style>
+    </>
+  )
 }
 
 export default Home
