5d5aa84046a89bb29ee6b53a8c1fe23aece4b56d
islandvalley <osm.shimaya@gmail.com>
2020-10-04 16:49:24 +0900

    スタイル設定の調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,21 @@
+import css from 'styled-jsx/css'
+
+const styles = css`
+  .main {
+    align-items: center;
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+  }
+`
+
 const Home = () => {
   return (
     <>
+      <style jsx>{styles}</style>
       <main className="main">
         <h1>🚧工事中</h1>
       </main>
-      <style jsx>{`
-        .main {
-          align-items: center;
-          display: flex;
-          height: 100vh;
-          justify-content: center;
-        }
-      `}</style>
     </>
   )
 }
